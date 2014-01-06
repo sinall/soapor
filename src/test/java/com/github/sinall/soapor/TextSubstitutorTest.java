10e79bc199371e279ce51b0122e3919d71cd409e
Gao Rui <gaoruinan@163.com>
2014-01-06 11:04:58 +0800

    Refactor - Change methods to normal ones instead of static ones.

---
+++ src/test/java/com/github/sinall/soapor/TextSubstitutorTest.java
@@ -0,0 +1,25 @@
+package com.github.sinall.soapor;
+
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class TextSubstitutorTest {
+
+    private TextSubstitutor textSubstitutor = new TextSubstitutor();
+
+    @Test
+    public void should_substitute_string() {
+        String template = "${username}@gmail.com";
+        Map<String, Object> params = new HashMap<String, Object>();
+        params.put("username", "Jack");
+
+        String email = textSubstitutor.substitute(template, params);
+
+        assertThat(email, is("Jack@gmail.com"));
+    }
+}

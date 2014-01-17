53e10fccca81e42c4372da84130d99726094e3d9
Gao Rui <gaoruinan@163.com>
2014-01-17 12:47:37 +0800

    Introduce soapor-matchers module.

--- soapor-core/src/main/java/com/github/sinall/soapor/util/SOAPMessageUtils.java
+++ soapor-core/src/main/java/com/github/sinall/soapor/util/SOAPMessageUtils.java
@@ -1,22 +1,13 @@
 package com.github.sinall.soapor.util;
 
 import org.apache.commons.io.IOUtils;
-import org.w3c.dom.Document;
 
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.soap.SOAPElement;
 import javax.xml.soap.SOAPMessage;
 import javax.xml.transform.OutputKeys;
 import javax.xml.transform.Source;
 import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
-import javax.xml.xpath.XPath;
-import javax.xml.xpath.XPathExpressionException;
-import javax.xml.xpath.XPathFactory;
-import java.io.ByteArrayInputStream;
-import java.io.InputStream;
 import java.io.StringWriter;
 
 public class SOAPMessageUtils {

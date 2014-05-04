6a2ce7902bf19b12956938bc758767014fb82af9
Gao Rui <gaoruinan@163.com>
2014-05-04 17:05:06 +0800

    Rename variable.

--- soapor-core/src/test/java/com/github/sinall/soapor/SOAPMessageFactoryTest.java
+++ soapor-core/src/test/java/com/github/sinall/soapor/SOAPMessageFactoryTest.java
@@ -28,10 +28,10 @@ public class SOAPMessageFactoryTest {
     @Test
     public void should_create_SOAPMessage_from_SOAPContext() throws SOAPException {
         String payloadName = "com/github/sinall/soapor/payload/example.xml";
-        SOAPParameters context = new SOAPParameters();
-        context.put("param1", 123);
+        SOAPParameters params = new SOAPParameters();
+        params.put("param1", 123);
 
-        SOAPMessage soapMessage = SOAPMessageFactory.create(payloadName, context);
+        SOAPMessage soapMessage = SOAPMessageFactory.create(payloadName, params);
 
         SOAPMessageEvaluator evaluator = new SOAPMessageEvaluator(soapMessage);
         assertThat(evaluator.getValueByXPath("/Envelope/Body/doubleAnInteger/param1"), is("123"));
